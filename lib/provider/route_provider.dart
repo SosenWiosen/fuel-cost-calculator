0ffe8b179f047e6b97212bf09fc0dc87c3a5950d
Przemek_Sosna <przemek.sosna@onet.pl>
2020-08-25 01:44:49 +0200

    added plugins etc

---
+++ lib/provider/route_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+
+class RouteProvider with ChangeNotifier{
+  List<Position> _pointsList=[];
+}
---
+++ lib/screens/route_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RouteScreem extends StatefulWidget {
+  @override
+  _RouteScreemState createState() => _RouteScreemState();
+}
+
+class _RouteScreemState extends State<RouteScreem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
